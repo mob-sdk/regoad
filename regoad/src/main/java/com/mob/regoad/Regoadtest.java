4d18a00374ff7434a22d68b67419dee09405ed05
o5728492 <312459028@qq.com>
2022-06-17 15:08:24 +0800

    first

---
+++ regoad/src/main/java/com/mob/regoad/Regoadtest.java
@@ -0,0 +1,16 @@
+package com.mob.regoad;
+
+/**
+ *
+ */
+public class Regoadtest {
+
+    /**
+     *
+     * @param number
+     * @return
+     */
+    public int calculation(int number){
+        return number*2;
+    }
+}
